1873716e70055bb606818e82aaea2e4f50faacaa
Benjamin Cohen <benjamin.cohen86@gmail.com>
2014-04-03 17:46:09 -0400

    Adds jQuery library and applications.js code

---
+++ application.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('#price-wrapper').on('keyup', '.quantity', function() {
+	//Get the price for blocks
+		var price = +$(this).closest('#price-wrapper').data('price');
+		//Get the quantity of blocks enter
+		var quantity = +$(this).val();
+		//Set the total of blocks * price
+		$('#total').text(price * quantity);
+
+});
+	$('.team-figure').on('mouseenter', 'img', function() {
+		$(this).find('.team-name').css('display', 'block').slideToggle();
+
+});
+});
